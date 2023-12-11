39b2694dfa2e89d7ea3c0da61c2736c997b6f109
95smile_ <57226469+codus1718@users.noreply.github.com>
2023-12-12 04:34:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import pandas as pd
 from time import sleep
 from tracemalloc import start
 from matplotlib import ticker
+from other_file import other_function
 
 st.set_page_config(
     page_icon="📊",
@@ -27,31 +28,32 @@ elif selected_page == "Other":
     other_function()
 
 
-st.title('GPV')
-st.write('The fluctuation of oil prices from April 15, 2008, to November 29, 2023.')
+def home():
+    st.title('GPV')
+    st.write('The fluctuation of oil prices from April 15, 2008, to November 29, 2023.')
 
 
 
-# CSV 파일 불러오기
-data = pd.read_csv("neural.csv")
+    # CSV 파일 불러오기
+    data = pd.read_csv("neural.csv")
 
-# 최대, 최소 날짜 설정
-max_date = pd.to_datetime('2023-11-29').date()
-min_date = pd.to_datetime('2008-04-15').date()
+    # 최대, 최소 날짜 설정
+    max_date = pd.to_datetime('2023-11-29').date()
+    min_date = pd.to_datetime('2008-04-15').date()
+    
+
+    start = st.date_input('Start Date', value=pd.to_datetime('2008-04-15'), min_value = min_date, max_value = max_date)
+    end = st.date_input('End Date',value=pd.to_datetime('2023-11-29'), min_value = min_date, max_value = max_date)
 
 
-start = st.date_input('Start Date', value=pd.to_datetime('2008-04-15'), min_value = min_date, max_value = max_date)
-end = st.date_input('End Date',value=pd.to_datetime('2023-11-29'), min_value = min_date, max_value = max_date)
+    if 'ds' in data.columns and 'y' in data.columns:
+        df = data[['ds', 'y']].copy()
+        df['ds'] = pd.to_datetime(df['ds'])
 
-
-if 'ds' in data.columns and 'y' in data.columns:
-    df = data[['ds', 'y']].copy()
-    df['ds'] = pd.to_datetime(df['ds'])
-
-    st.line_chart(df.set_index('ds').loc[start:end])
-else:
-    st.warning("CSV 파일은 'ds'와 'y' 열을 포함해야 합니다.")
-    None
+        st.line_chart(df.set_index('ds').loc[start:end])
+    else:
+        st.warning("CSV 파일은 'ds'와 'y' 열을 포함해야 합니다.")
+        None
     
 
 
