f9c4b363dd3d7a91bf51da6c7a8f87d717ab4e55
95smile_ <57226469+codus1718@users.noreply.github.com>
2023-12-12 08:40:15 +0900

    Update Types of Crude Oil.py

--- pages/Types of Crude Oil.py
+++ pages/Types of Crude Oil.py
@@ -14,11 +14,11 @@ st.title('Combined Price Trends of WTI, Brent, Dubai, and Gasoline')
 
 
 # 두 개의 CSV 파일을 읽어옴
-df1 = pd.read_csv('file1.csv')
-df2 = pd.read_csv('file2.csv')
+df1 = pd.read_csv('oil.csv')
+df2 = pd.read_csv('gasoline.csv')
 
 # 두 데이터프레임을 공통된 열을 기준으로 병합
-merged_df = pd.merge(df1, df2, on='common_column_name', how='inner')
+merged_df = pd.merge(df1, df2, on='Date', how='inner')
 
 # 시각화
 fig = px.line(merged_df, x='Date', y=['WTI', 'Brent', 'Dubai', 'Gasoline'],
