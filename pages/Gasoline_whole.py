ca4a971b99f4a6d05699b4f34722c26b351fb322
95smile_ <57226469+codus1718@users.noreply.github.com>
2023-12-12 13:12:36 +0900

    Update Gasoline_whole.py

--- pages/Gasoline_whole.py
+++ pages/Gasoline_whole.py
@@ -12,6 +12,10 @@ st.set_page_config(
 )
 
 st.title('Comparison of Oil Prices among Refinery, Distributor, Agent, and Gas Statio')
+st.write('세전가 : 어떤 상품이나 서비스의 가격에 세금이나 부가가치세(VAT)를 포함하지 않은 가격')
+st.write('정유사 :  정제된 석유 제품을 생산하는 회사나 시설을 가리키는 한국어 용어')
+
+
 
 
 fig = px.line(df, x='Date', y=['세전가',	'정유사', '대리점', '주유소'],
