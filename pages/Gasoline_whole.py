8d00a9a8aea1351b53e87a92f45b8d940db7e2d8
95smile_ <57226469+codus1718@users.noreply.github.com>
2023-12-12 10:47:10 +0900

    Update Gasoline_whole.py

--- pages/Gasoline_whole.py
+++ pages/Gasoline_whole.py
@@ -5,17 +5,17 @@ import plotly.express as px
 
 df = pd.read_csv('gasoline_whole.csv')
 
+st.title('--아직--')
 
 st.set_page_config(
     page_icon="📊",
-    page_title="Tax Visualization",
+    page_title="Gasoline_whole",
     layout="wide"
 )
 
 
 fig = px.line(df, x='Date', y=['세전가',	'정유사', '대리점', '주유소'],
-              labels={'value': 'Gasoline Price (KRW/L)'},
-              title='Tax Components Over Time')
+              labels={'value': 'Gasoline Price (KRW/L)'})
 
 
 st.plotly_chart(fig, use_container_width=True)
