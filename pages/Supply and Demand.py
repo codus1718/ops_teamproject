d42e79345cd5020984e3395d087a39fc71294979
95smile_ <57226469+codus1718@users.noreply.github.com>
2023-12-12 14:18:05 +0900

    Update Supply and Demand.py

--- pages/Supply and Demand.py
+++ pages/Supply and Demand.py
@@ -15,7 +15,7 @@ df = pd.read_csv("supply_demand_refined.csv")
 
 
 # 선 그래프 시각화
-fig = px.line(df, x='Date', y=['Oil Import', 'Supply', 'Demand', 'Total Inventory'],
+fig = px.bar(df, x='Date', y=['Oil Import', 'Supply', 'Demand', 'Total Inventory'],
               labels={'value': 'Amount of Oil(1k Bbl)'})
 
 # 선 그래프를 streamlit에 표시
