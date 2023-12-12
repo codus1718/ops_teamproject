3ec4c9fe60299be78555eac5093dd5c304b408dc
95smile_ <57226469+codus1718@users.noreply.github.com>
2023-12-12 13:06:52 +0900

    Update Types of Crude Oil.py

--- pages/Types of Crude Oil.py
+++ pages/Types of Crude Oil.py
@@ -26,3 +26,9 @@ fig = px.line(merged_df, x='Date', y=['WTI', 'Brent', 'Dubai', 'Gasoline'],
 
 # 시각화 보여주기
 st.plotly_chart(fig, use_container_width=True)
+
+st.write('WTI (West Texas Intermediate): 미국 텍사스 주 서부 지역에서 채취되는 원유. WTI는 가장 널리 거래되는 석유 지표 중 하나이며, 미국에서 소비되는 석유 제품 가격에 영향을 미침.')
+
+st.write('Brent: 북해 지역에서 채취되는 원유. Brent는 유럽과 아시아 지역에서 사용되는 주요 석유 지표 중 하나이며, 국제적으로 중요한 기준 지표 중 하나로 간주됨.')
+
+st.write('Dubai: 중동 지역에서 채취되는 원유. Dubai 원유는 주로 중동 지역에서 소비되는 석유 제품 가격을 결정하는 데 사용됨')
